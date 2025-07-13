23d3c9aa96a3199c30f855676fe74cf4f068a694
C-Orta <colbertorta@gmail.com>
2025-07-13 16:57:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-console.log('test1');
-const testParameters = 'key=key&value=value';
-const x = new URLSearchParams(testParameters);
-
-return x;
-console.log(testParameters);
-console.log('hi');
+console.log('script.js is working');
